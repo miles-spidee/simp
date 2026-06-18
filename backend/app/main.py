4f8bbde7f6f95408d65a232ecde327e1f1a39394
Joshua-Melvin-2024-aia <melvinjoshua777@gmail.com>
2026-06-18 14:12:15 +0530

    main.py py updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 
-# App initialization
-app = FastAPI()
+from app.modules.auth.router import router as auth_router
+from app.modules.internship_types.router import router as internship_router
+from app.modules.applications.router import router as applications_router
+from app.modules.application_documents.router import router as application_documents_router
+from app.modules.application_profiles.router import router as application_profiles_router
+from app.modules.corporate_application_details.router import router as corporate_application_details_router
+from app.modules.industrial_application_details.router import router as industrial_application_details_router
+from app.modules.paid_application_details.router import router as paid_application_details_router
+from app.modules.research_application_details.router import router as research_application_details_router
+from app.modules.stipend_application_details.router import router as stipend_application_details_router
+
+app = FastAPI(
+    title="Pinesphere ERP"
+)
+
+app.include_router(auth_router)
+app.include_router(internship_router)
+app.include_router(applications_router)
+app.include_router(application_documents_router)
+app.include_router(application_profiles_router)
+app.include_router(corporate_application_details_router)
+app.include_router(industrial_application_details_router)
+app.include_router(paid_application_details_router)
+app.include_router(research_application_details_router)
+app.include_router(stipend_application_details_router)
 
 # # Example Route
 # @app.get("/check") # We can use app.post() also for POST requests
