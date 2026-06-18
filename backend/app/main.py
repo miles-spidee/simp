fc97ac07fc982c80a2ed10f515db7ae09e373143
ARUNAWRISHE <arunawrishe@gmail.com>
2026-06-18 22:42:44 +0530

    refactor: remove entire backend authentication and internship modules

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# App initialization
-app = FastAPI()
-
-# # Example Route
-# @app.get("/check") # We can use app.post() also for POST requests
-# def health_check():
-#     return {"status": "ok !!"}
