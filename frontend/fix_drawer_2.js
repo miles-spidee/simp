bc00864f28b2fb3fc277a19bbf40a0738c0a0a66
ARUNAWRISHE <arunawrishe@gmail.com>
2026-06-28 13:28:20 +0530

    chore: remove temporary build and patch scripts from frontend directory

--- frontend/fix_drawer_2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const file = '/Users/test/Documents/simp/frontend/components/admin/application/ReviewApplicationDrawer.tsx';
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/<Drawer isOpen=\{isOpen\} onClose=\{onClose\} size="lg">/g, '<Drawer isOpen={isOpen} onClose={onClose} size="lg" title="Review Application">');
-fs.writeFileSync(file, content);
-console.log(`Updated ${file}`);
